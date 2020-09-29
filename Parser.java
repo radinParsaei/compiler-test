489938da347ed0702258e2bb8f6d780cf20547a8
radinParsaei <radinparsaei@gmail.com>
2020-09-29 21:56:29 +0330

    regex support added to the parser

--- Parser.java
+++ Parser.java
@@ -1,5 +1,6 @@
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 	public interface CompilerLambda {
@@ -43,19 +44,13 @@ public class Parser {
 
 	public void on(String model, String newName, CompilerLambda lambda) {//replace previous model with newName and store lambda output on it Object
 		String map = this.getMap() + " ";
-		String[] models = model.split("\\|");
-		int index = 0;
-		for (int i = 0; i < models.length; i++) {
-			models[i] = models[i].trim() + " ";
-			index = map.indexOf(models[i]);
-			if (index != -1) {
-				model = models[i];
-				break;
-			}
+		Pattern	pattern = Pattern.compile(model + " ");
+		Matcher matcher = pattern.matcher(map);
+		if (!matcher.find()) {
+			return ;
 		}
-		if (index < 0) {
-			return;
-		}
+		String matched = matcher.group(0);
+		int index = map.indexOf(matched);
 		int listIndex = 0;
 		for(int i = 0; i < index; i++) {
 			if(map.charAt(i) == ' ') {
@@ -66,7 +61,7 @@ public class Parser {
 		if (saveTexts) text = new StringBuilder();
 		int tmp = listIndex;
 		ArrayList<Token> tmpTokens = new ArrayList<>();
-		for(int i = 0; i < model.split(" ").length; i++) {
+		for(int i = 0; i < matched.split(" ").length; i++) {
 			tmpTokens.add(tokens.get(listIndex + i));
 			if (saveTexts) text.append(tokens.get(listIndex + i).getText());
 			tokens.remove(listIndex + i);
@@ -78,69 +73,13 @@ public class Parser {
 		tokens.add(listIndex, t);
 		if (!singleRun) {
 			if (singleRunPerLocation) {
-				for (String model2 : models) {
-					if (map.indexOf(model2, index + 1) != -1) {
-						this.on(model, newName, lambda);
-					}
+				String map1 = map.substring(index);
+				Matcher matcher1 = pattern.matcher(map1);
+				if (matcher1.find()) {
+					this.on(model, newName, lambda);
 				}
 			} else {
-				for (String model2 : models) {
-					if (map.contains(model2)) {
-						this.on(model, newName, lambda);
-					}
-				}
-			}
-		}
-	}
-
-	public void on(String model, String newName, Object parentOfMethod, String methodName) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {//replace previous model with newName and store method output on it Object
-		String map = this.getMap() + " ";
-		String[] models = model.split("\\|");
-		int index = 0;
-		for (int i = 0; i < models.length; i++) {
-			models[i] = models[i].trim() + " ";
-			index = map.indexOf(models[i]);
-			if (index != -1) {
-				model = models[i];
-				break;
-			}
-		}
-		if (index < 0) {
-			return;
-		}
-		int listIndex = 0;
-		for(int i = 0; i < index; i++) {
-			if(map.charAt(i) == ' ') {
-				listIndex++;
-			}
-		}
-		StringBuilder text = null;
-		if (saveTexts) text = new StringBuilder();
-		int tmp = listIndex;
-		ArrayList<Token> tmpTokens = new ArrayList<>();
-		for(int i = 0; i < model.split(" ").length; i++) {
-			tmpTokens.add(tokens.get(listIndex + i));
-			if (saveTexts) text.append(tokens.get(listIndex + i).getText());
-			tokens.remove(listIndex + i);
-			listIndex--;
-		}
-		listIndex = tmp;
-		Token t = new Token(newName, saveTexts? text.toString():null);
-		t.setObject(parentOfMethod.getClass().getMethod(methodName, Parser.class).invoke(parentOfMethod, new Parser(tmpTokens)));
-		tokens.add(listIndex, t);
-		if (!singleRun) {
-			if (singleRunPerLocation) {
-				for (String model2 : models) {
-					if (map.indexOf(model2, index + 1) != -1) {
-						this.on(model, newName, parentOfMethod, methodName);
-					}
-				}
-			} else {
-				for (String model2 : models) {
-					if (map.contains(model2)) {
-						this.on(model, newName, parentOfMethod, methodName);
-					}
-				}
+				this.on(model, newName, lambda);
 			}
 		}
 	}
